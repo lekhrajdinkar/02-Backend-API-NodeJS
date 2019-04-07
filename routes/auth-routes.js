464254e49e8ec972c95bca1926ed704e9a7b83be
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-06 18:44:51 -0700

    signup and login

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const Joi = require('joi');
 const {check} = require('express-validator/check');
 const authController = require('./Controller/auth-controller');
+const tactMongoDB = require('./../util/mongoDB').db;
+const bcrypt = require('bcryptjs');
 
-//================ TEST ============
+//================ TEST ============ START
 router.use('/welcome',(req,resp,next)=> {
     if(req.session.currentUser)
         resp.send('<html><h1>welcome to TACT</h1></html>');
@@ -16,16 +18,56 @@ router.use('/status2',(req,resp,next)=> {
     resp.send({status : 'running'});
 });
 
+
+//====== LOGIN =========
 router.post('/login',(req,resp,next)=> {
     //req.session.isloggedIN=true;
-    
-    //authenticate and on success store current user in session
-    req.session.currentUser = { initial: req.body.initial, name : req.body.name};
-
-    resp.redirect('./welcome')
+    tactMongoDB().collection('login').find({ initial: req.body.initial}).toArray()
+    .then((user) => {
+        if(user) {
+            return bcrypt.compare(req.body.password, user[0].password)
+        }
+    })
+    .then((match) => {
+        if(match){
+            req.session.currentUser = { initial: req.body.initial};
+            resp.redirect('./welcome');
+        }else{
+            resp.json({ initial: req.body.initial, login_status : "FAILED" }); 
+        }
+    })
+    .catch((err) => { 
+        console.log(err);
+        resp.json({ initial: req.body.initial, login_status : "FAILED" }); 
+    });
 });
 
-//================ TACT ===========
+//===== SIGNUP ==========
+router.post('/signup',(req,resp,next)=> {
+    tactMongoDB().collection('login').find({ initial: req.body.initial}).toArray()
+    .then((user) => {
+       if(!user)
+            return resp.json({ initial: req.body.initial, signup_status : "ALREADY SIGNED UP" }); 
+        else
+            return bcrypt.hash(req.body.password, 12);
+    })
+    .then((hashedpswd) => {
+        tactMongoDB().collection('login').insertOne({ initial: req.body.initial, password : hashedpswd})
+    })
+    .then(() => { 
+        console.log('signup successfully... : ', req.body.initial); 
+        resp.json({ initial: req.body.initial, signup_status : "SUCCESS" });
+    })
+    .catch((err) => { 
+        console.log(err);
+        resp.json({ initial: req.body.initial, signup_status : "FAILED" }); 
+    });
+
+});
+//================ TEST ============ END
+
+
+//================ TACT APP===========
 
 
 //1 GET with express body validator --> it will set valiation error in req.error property
