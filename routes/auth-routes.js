6c0406b7721e1c3a60a37f1f140a6276645072b6
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-07 01:51:27 -0700

    JWT :)
    so eaily to implemet in nodejs world as campared to java world.

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -5,6 +5,9 @@ const {check} = require('express-validator/check');
 const authController = require('./Controller/auth-controller');
 const tactMongoDB = require('./../util/mongoDB').db;
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const jwtMwe = require('./../util/mwe/jwt-mwe')
 
 //================ TEST ============ START
 router.use('/welcome',(req,resp,next)=> {
@@ -23,32 +26,55 @@ router.use('/status2',(req,resp,next)=> {
 });
 
 
-//====== LOGIN =========
+//====== LOGIN /LOGUT/ SIGNUP=========
 router.get('/logout',(req,resp,next)=> {
     req.session.destroy();
     resp.status(200).json({ initial: req.body.initial, logout_status : "SUCCESSFUL" })
 });
 
 router.post('/login',(req,resp,next)=> {
-    tactMongoDB().collection('login').find({ initial: req.body.initial}).toArray()
+    let user_temp;
+    const user_req = req.body.initial || 'AAXL';
+    const pswd_req = req.body.password || '123456';
+
+    tactMongoDB().collection('login').find({ initial: user_req}).toArray()
     .then((user) => {
+        user_temp = user[0];
         if(user[0]) 
-            return bcrypt.compare(req.body.password, user[0].password) //compare
+            return bcrypt.compare(pswd_req, user[0].password) //compare
     })
     .then((match) => {
         if(match){
-            req.session.currentUser = { initial: req.body.initial};
-            resp.status(200).json({initial: req.body.initial, login_status : "SUCCESSFUL"});
+            req.session.currentUser = { initial: user_req};
+
+            //=== JWT ===
+            //1. Generate JWT
+            const jwtToken = jwt.sign({
+                initial: user_req,
+                _id : user_temp._id,
+                dt: Date.now()
+            }, 
+            config.get('jwt.secret'),
+            {expiresIn:'1h'}
+            );
+            //req.session.jwt  = jwtToken;
+
+            //2. send JWT to client inresponse.
+            resp.status(200).json({jwt: jwtToken, initial: user_req, login_status : "SUCCESSFUL"});
+
+            //3. NEW MWE to validate JWT token before processing request. check app.js.
+
         }else{
             error = new Error();
+            error.status = 401;
             error.message = "incorrect login credential";
-            error.data = { initial: req.body.initial, login_status : "FAILED"}
+            error.data = { initial: user_req, login_status : "FAILED"}
             next(error);
         }
     })
     .catch((err) => { 
         console.log(err);
-        resp.json({ initial: req.body.initial, login_status : "FAILED" }); 
+        resp.json({ initial: user_req, login_status : "FAILED" }); 
     });
 });
 
@@ -85,7 +111,9 @@ router.post('/signup',(req,resp,next)=> {
 
 
 //1 GET with express body validator --> it will set valiation error in req.error property
-router.post("/add-user",
+router.post(
+    "/add-user",
+    jwtMwe, //adding middleware
 [
     check('name').trim().isLength({min : 3}).isAlphanumeric().withMessage("minimun lenght is 3 and should not be alphanumeric"),
     check('initial').trim().isLength({min : 3}).custom((value, {req}) => {
---
+++ util/mwe/jwt-mwe.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+//high level function of this module
+module.exports = (req,resp,next) => {
+    let token ;
+    let decodedToken;
+
+    //1. get auth header;
+    const authHeader = req.get('Authorization')
+    if(!authHeader){
+        error = new Error();
+        error.status = 500;
+        error.message = 'Authentication failed : JWT verification failed';
+        error.data = { cause: 'Authorization header not been set'};
+        next(error);
+    }
+
+    //2. get jwt token from auth header and verify token
+    try{
+        token  = authHeader.split(' ')[1];
+        decodedToken = jwt.verify(token, config.get('jwt.secret'))
+        console.log('decodedToken : ', decodedToken)
+    }
+    catch(err){
+        err.status = 500;
+        err.data = err.message;
+        err.message = "error while validating JWT token";      
+        throw err;
+    }
+
+    //3 verificatin failed
+    if(!decodedToken){
+            error = new Error('Authentication Failed');
+            error.status = 500;
+            error.message = "JWT verification failed";
+            //error.data = { initial: user_req, login_status : "FAILED"}
+            next(error);
+    }else{
+        console.log('JWT authenticaton passed. user dont need to login again for each REST.');
+        next();
+    }
+}
